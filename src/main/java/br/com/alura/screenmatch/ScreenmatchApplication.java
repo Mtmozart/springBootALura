c7b434dc2c4904b24a8fcebde42ab574e2180bf9
matheus mozart <mmsnborges@gmail.com>
2023-12-27 10:07:28 -0400

    modelando episodios

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumerApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -21,6 +22,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados =	conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		System.out.println("----------------------");
+		var json2 = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json2, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 
 
 	}
